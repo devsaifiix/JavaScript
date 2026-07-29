944ac6e4d6f85f86b68b1c38492fb3e12512cb43
devsaifiix <devsaifiix@gmail.com>
2026-07-29 13:56:43 +0500

    Initial Commit

---
+++ 03.Js Operators/02.Assignment/assignment.js
@@ -0,0 +1,21 @@
+let x = 10;
+
+document.write("Initial value of x: ", x + "<br>");
+
+x += 5;
+document.write("x += 5: ", x + "<br>");
+
+x -= 5;
+document.write("x -= 5: ", x + "<br>");
+
+x *= 2;
+document.write("x *= 2: ", x + "<br>");
+
+x /= 4;
+document.write("x /= 4: ", x + "<br>");
+
+x %= 3;
+document.write("x %= 3: ", x + "<br>");
+
+x **= 2;
+document.write("x **= 2: ", x + "<br>");
