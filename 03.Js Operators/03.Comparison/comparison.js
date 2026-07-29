3837c71a68425f059e7fd5dce6b72862d0009696
devsaifiix <devsaifiix@gmail.com>
2026-07-29 14:03:19 +0500

    Initial Commit

---
+++ 03.Js Operators/03.Comparison/comparison.js
@@ -0,0 +1,31 @@
+let x = 10;
+let y = 20;
+let z = "10";
+
+document.write("x = ", x + "<br>");
+document.write("y = ", y + "<br>");
+document.write('z = "', z, '" (String)<br><br>');
+
+let equal = x == z;
+document.write("x == z: ", equal + "<br>");
+
+let strictEqual = x === z;
+document.write("x === z: ", strictEqual + "<br>");
+
+let notEqual = x != y;
+document.write("x != y: ", notEqual + "<br>");
+
+let strictNotEqual = x !== z;
+document.write("x !== z: ", strictNotEqual + "<br>");
+
+let greaterThan = x > y;
+document.write("x > y: ", greaterThan + "<br>");
+
+let lessThan = x < y;
+document.write("x < y: ", lessThan + "<br>");
+
+let greaterThanEqual = x >= 10;
+document.write("x >= 10: ", greaterThanEqual + "<br>");
+
+let lessThanEqual = x <= y;
+document.write("x <= y: ", lessThanEqual + "<br>");
