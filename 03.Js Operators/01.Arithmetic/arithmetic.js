5fc3d96bdcc57aea97397290807d6277dc4d854f
devsaifiix <devsaifiix@gmail.com>
2026-07-29 13:41:26 +0500

    Initial Commit

---
+++ 03.Js Operators/01.Arithmetic/arithmetic.js
@@ -0,0 +1,29 @@
+let x = 5;
+let y = 2;
+
+let addition = x + y;
+document.write("x + y: ", addition + "<br>" )
+
+let subtraction = x - y;
+document.write("x - y: ", subtraction + "<br>" )
+
+let multiplication = x * y;
+document.write("x + y: ", multiplication + "<br>" )
+
+let division = x / y;
+document.write("x / y: ", division + "<br>" )
+
+let modulus = x % y;
+document.write("x % y: ", modulus + "<br>")
+
+x++;
+let increment = x;
+document.write("Incremental x is: ", increment + "<br>")
+
+y--;
+let decrement = y;
+document.write("Decremental y is: ", decrement + "<br>" )
+
+let power = Math.pow(x, y);   // ya x ** y
+document.write("x ** y: ", power)
+
