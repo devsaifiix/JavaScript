8f8b9d85b7c9d654677978bf4b4bd128e6d135f8
devsaifiix <devsaifiix@gmail.com>
2026-07-31 23:01:52 +0500

    Initial Commit

--- 01.Outputs/outputs.js
+++ 01.Outputs/outputs.js
@@ -1,20 +1,20 @@
-// 1. innerHTML
+// 1. Output with innerHTML
 document.getElementById("demo").innerHTML = "<h2>Hello World using innerHTML</h2>";
 
-// 2. innerText
+// 2. Output with innerText
 // document.getElementById("demo").innerText = "Hello World using innerText";
 
-// 3. document.write()
+// 3. Output with document.write()
 function writeOutput() {
     document.write("5 + 6 = " + (5 + 6));
 }
 
-// 4. alert()
+// 4. Output with alert()
 function alertOutput() {
     alert("5 + 6 = " + (5 + 6));
 }
 
-// 5. console.log()
+// 5. Output with console.log()
 function consoleOutput() {
     console.log("5 + 6 = " + (5 + 6));
 }
