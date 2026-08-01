273265e2e11bafd55b64e1e9f38c33a428e4b2ff
devsaifiix <devsaifiix@gmail.com>
2026-08-01 06:43:40 +0500

    Second Commit

--- 03.Js Operators/01.Arithmetic/arithmetic.js
+++ 03.Js Operators/01.Arithmetic/arithmetic.js
@@ -1,29 +1,46 @@
+// Arithmetic Operators in JavaScript
+
+document.write("<h2>Arithmetic Operators in JavaScript</h2>");
+
 let x = 5;
 let y = 2;
 
+// Addition
 let addition = x + y;
-document.write("x + y: ", addition + "<br>" )
+document.write("Addition (x + y): " + addition + "<br>");
 
+// Subtraction
 let subtraction = x - y;
-document.write("x - y: ", subtraction + "<br>" )
+document.write("Subtraction (x - y): " + subtraction + "<br>");
 
+// Multiplication
 let multiplication = x * y;
-document.write("x + y: ", multiplication + "<br>" )
+document.write("Multiplication (x * y): " + multiplication + "<br>");
 
+// Division
 let division = x / y;
-document.write("x / y: ", division + "<br>" )
+document.write("Division (x / y): " + division + "<br>");
 
+// Modulus
 let modulus = x % y;
-document.write("x % y: ", modulus + "<br>")
+document.write("Modulus (x % y): " + modulus + "<br>");
 
+// Increment
 x++;
 let increment = x;
-document.write("Incremental x is: ", increment + "<br>")
+document.write("Increment (++x): " + increment + "<br>");
 
+// Decrement
 y--;
 let decrement = y;
-document.write("Decremental y is: ", decrement + "<br>" )
+document.write("Decrement (--y): " + decrement + "<br>");
 
-let power = Math.pow(x, y);   // ya x ** y
-document.write("x ** y: ", power)
+// Exponentiation using Math.pow()
+let power = Math.pow(x, y);
+document.write("Exponentiation (Math.pow(x, y)): " + power + "<br>");
 
+// Exponentiation using **
+let power2 = x ** y;
+document.write("Exponentiation (x ** y): " + power2 + "<br>");
+
+document.write("<hr>");
