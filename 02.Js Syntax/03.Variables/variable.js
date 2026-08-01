38c10092c4742cc766650cc350e6b63db417f2e5
devsaifiix <devsaifiix@gmail.com>
2026-08-01 05:57:37 +0500

    Second Commit

--- 02.Js Syntax/03.Variables/variable.js
+++ 02.Js Syntax/03.Variables/variable.js
@@ -1,12 +1,24 @@
+// Example using var
+document.write("<h2>1. Varibales in js by using var</h2>")
+var d = "Saif Tahir";
+var age = 34;
+// var age = 50;   // you can redeclare a variable by using var
+age = 25;   // var can be override the value of age
+document.write("Your name is: ", d + "<br>", "Your age is: ", age);
+
 // Example using let
+document.write("<h2>2. Varibales in js by using let</h2>")
 let x = 5;
-let y = 6;
-let z = x + y;
+// let x = 6;      // you can't redeclare a variable by using let
+x = 6;      // but you can reassign a value a to variable by using let
+document.write( "Value of x is: ", x)
 
 // Example using const
+document.write("<h2>3. Varibales in js by using const</h2>")
 const a = 5;
-const b = 6;
-const c = x + y;
+// const a = 10; // you can't redeclare a variable by using const
+// a = 20; // also you can't reassign a value to a variable by using const
+document.write("Value of a is: ", a);
 
 let _lastName = "Tahir";
 
